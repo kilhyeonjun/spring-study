51b82e1cfd38139932afa321a01e481c684c9d1e
kilhyeonjun <kboxstar@gmail.com>
2021-09-26 00:08:10 +0900

    RoleType 생성

---
+++ src/main/java/com/kbox/blog/model/RoleType.java
@@ -0,0 +1,5 @@
+package com.kbox.blog.model;
+
+public enum RoleType {
+	USER, ADMIN
+}
--- src/main/java/com/kbox/blog/model/User.java
+++ src/main/java/com/kbox/blog/model/User.java
@@ -4,11 +4,12 @@ import java.sql.Timestamp;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 
 import lombok.AllArgsConstructor;
@@ -37,8 +38,9 @@ public class User {
 		@Column(nullable =false, length = 50)
 		private String email;
 
-		@ColumnDefault("'user'")
-		private String role;  // Enum을 쓰는게 좋다. // admin, user, manager
+		// DB는 RoleType이라는게 없다.
+		@Enumerated(EnumType.STRING)
+		private RoleType role;  // Enum을 쓰는게 좋다. // admin, user, manager
 	
 		@CreationTimestamp // 시간이 자동 입력
 		private Timestamp createDate;
