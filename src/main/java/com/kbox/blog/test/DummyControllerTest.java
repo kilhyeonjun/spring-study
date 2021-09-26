b0f4839f471531880aacfed596e13bc19a8f4362
kilhyeonjun <kboxstar@gmail.com>
2021-09-26 23:47:53 +0900

    user 전체 데이터 검색, pasing

--- src/main/java/com/kbox/blog/test/DummyControllerTest.java
+++ src/main/java/com/kbox/blog/test/DummyControllerTest.java
@@ -1,8 +1,13 @@
 package com.kbox.blog.test;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +22,24 @@ public class DummyControllerTest {
 
 	@Autowired // 의존성 주입(DI)
 	private UserRepository userRepository;
+	
+	// http://localhost:8000/blog/dummy/users
+	@GetMapping("/dummy/users")
+	public List<User> list(){
+		return userRepository.findAll();
+	}
+	
+
+	// http://localhost:8000/blog/dummy/user/page
+	// http://localhost:8000/blog/dummy/user?page=1
+	// 한페이지당 2건에 데이터를 리턴받아 볼 예정
+	@GetMapping("/dummy/user")
+	public List<User> pageList(@PageableDefault(size = 2, sort = "id", direction = Sort.Direction.DESC) Pageable pageable){
+		Page<User> pagingUser = userRepository.findAll(pageable);
+		
+		List<User> users = pagingUser.getContent();
+		return users;
+	}
 	
 	// {id} 주소로 파마레터를 전달 받을 수 있음.
 	// http://localhost:8000/blog/dummy/user/3
@@ -34,6 +57,11 @@ public class DummyControllerTest {
 			}
 		});
 		//v2
+		// 람다식
+		
+		User user = userRepository.findById(id).orElseThrow(()->{
+			return new IllegalArgumentException("해당 유저는 없습니다. id : " + id);
+		});
 		*/
 		User user = userRepository.findById(id).orElseThrow(new Supplier<IllegalArgumentException>() {
 			@Override
@@ -42,12 +70,12 @@ public class DummyControllerTest {
 				return new IllegalArgumentException("해당 유저는 없습니다. id : " + id);
 			}
 		});
-		// 람다식
-		/*
-		User user = userRepository.findById(id).orElseThrow(()->{
-			return new IllegalArgumentException("해당 유저는 없습니다. id : " + id);
-		});
-		*/
+		// 요청 : 웹브라우저
+		// user 객체 = 자바 오브젝트
+		// 변환 (웹브라우저가 이해할 수 있는 데이터) -> json
+		// 기본 스프링 Gson 라이브러리 사용
+		// 스프링부트 = MessageConverter라는 애가 응답시에 자동 작동
+		// 자바 오브젝트 리턴시 MessageConverter가 자바 오브젝트를 json으로 변환해서 던져줌
 		return user;
 	}
 	
