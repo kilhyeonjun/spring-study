c3ba9ceb09db9e9bc1c2b23718ef7420334eb2fb
kilhyeonjun <kboxstar@gmail.com>
2021-09-26 23:16:54 +0900

    user 검색 추가

--- src/main/java/com/kbox/blog/test/DummyControllerTest.java
+++ src/main/java/com/kbox/blog/test/DummyControllerTest.java
@@ -1,6 +1,10 @@
 package com.kbox.blog.test;
 
+import java.util.function.Supplier;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +17,33 @@ public class DummyControllerTest {
 
 	@Autowired // 의존성 주입(DI)
 	private UserRepository userRepository;
+	
+	// {id} 주소로 파마레터를 전달 받을 수 있음.
+	// http://localhost:8000/blog/dummy/user/3
+	@GetMapping("/dummy/user/{id}")
+	public User detail(@PathVariable int id) {
+		// 없는 값을 찾으면 user의 값이 null 이 됨
+		// User user = userRepository.findById(id); //v1
+		// Optional로 user 객체를 감싸서 null 유무 판단
+		/*
+		User user = userRepository.findById(id).orElseGet(new Supplier<User>() {
+			@Override
+			public User get() {
+				// TODO Auto-generated method stub
+				return new User();
+			}
+		});
+		//v2
+		*/
+		User user = userRepository.findById(id).orElseThrow(new Supplier<IllegalArgumentException>() {
+			@Override
+			public IllegalArgumentException get() {
+				// TODO Auto-generated method stub
+				return new IllegalArgumentException("해당 유저는 없습니다. id : " + id);
+			}
+		});
+		return user;
+	}
 	
 	// http://localhost:8000/blog/dummy/join (요청)
 	// http의 body에 username, password, email 데이터를 가지고 (요청)
