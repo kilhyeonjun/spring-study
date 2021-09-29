1448ca94471f3692df6d009ae3f9d29511ab6a4d
kilhyeonjun <kboxstar@gmail.com>
2021-09-29 17:59:39 +0900

    Response 수정

--- src/main/java/com/kbox/blog/controller/api/UserApiController.java
+++ src/main/java/com/kbox/blog/controller/api/UserApiController.java
@@ -22,7 +22,7 @@ public class UserApiController {
 		System.out.println("UserApiController : save 호출됨");
 		// 실제로 DB에 insert를 하고 아래에서 return
 		user.setRole(RoleType.USER);
-		int result = userService.회원가입(user);
-		return new ResponseDto<Integer>(HttpStatus.OK, result); // 자바오브젝트를 JSON으로 변환해서 리턴 (Jackson)
+		userService.회원가입(user);
+		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1); // 자바오브젝트를 JSON으로 변환해서 리턴 (Jackson)
 	}
 }
