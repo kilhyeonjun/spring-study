12cb0b736c29b606ad91dae23e0ff2c89b445a0b
kilhyeonjun <kboxstar@gmail.com>
2021-09-30 21:08:12 +0900

    spring security 적용

--- src/main/java/com/kbox/blog/controller/api/UserApiController.java
+++ src/main/java/com/kbox/blog/controller/api/UserApiController.java
@@ -19,9 +19,6 @@ public class UserApiController {
 	@Autowired
 	private UserService userService;
 	
-	@Autowired
-	private HttpSession session;
-	
 	@PostMapping("/api/user")
 	public ResponseDto<Integer> save(@RequestBody User user) { // username, password, email
 		System.out.println("UserApiController : save 호출됨");
@@ -31,9 +28,10 @@ public class UserApiController {
 		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1); // 자바오브젝트를 JSON으로 변환해서 리턴 (Jackson)
 	}
 	
+	/*
 	// 다음시간에 스프링 시큐리티 이용해서 로그인!!
 	@PostMapping("/api/user/login")
-	public ResponseDto<Integer> login(@RequestBody User user){
+	public ResponseDto<Integer> login(@RequestBody User user, HttpSession session){
 		System.out.println("UserApiController : login 호출됨");
 		User principal = userService.로그인(user); // principal (접근 주체)
 		
@@ -42,4 +40,5 @@ public class UserApiController {
 		}
 		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
 	}
+	*/
 }
