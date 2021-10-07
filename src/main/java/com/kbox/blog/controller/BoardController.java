a006b7b40fa34c6ab661dffcc434530a786f3074
kilhyeonjun <kboxstar@gmail.com>
2021-10-07 22:27:51 +0900

    글 목록 기능 구현

--- src/main/java/com/kbox/blog/controller/BoardController.java
+++ src/main/java/com/kbox/blog/controller/BoardController.java
@@ -3,18 +3,24 @@ package com.kbox.blog.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.kbox.blog.config.auth.PrincipalDetail;
+import com.kbox.blog.service.BoardService;
 
 @Controller
 public class BoardController {
 
+	@Autowired
+	private BoardService boardService;
+	
 	// @AuthenticationPrincipal PrincipalDetail principal
 	@GetMapping({"","/"})
-	public String index() { // 컨트롤러에서 세션을 어떻게 찾는지?
+	public String index(Model model) { // 컨트롤러에서 세션을 어떻게 찾는지?
+		model.addAttribute("boards", boardService.글목록());
 		// /WEB-INF/views/index.jsp
-		return "index";
+		return "index"; // viewResolver 작동!!
 	}
 	
 	// USER 권환이 필요
--- src/main/java/com/kbox/blog/service/BoardService.java
+++ src/main/java/com/kbox/blog/service/BoardService.java
@@ -1,5 +1,7 @@
 package com.kbox.blog.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,5 +22,9 @@ public class BoardService {
 		board.setCount(0);
 		board.setUser(user);
 		boardRepository.save(board);
+	}
+
+	public List<Board> 글목록() {
+		return boardRepository.findAll();
 	}
 }
