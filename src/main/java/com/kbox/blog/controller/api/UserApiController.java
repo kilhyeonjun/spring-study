b69cd7309e8e122f0a85fbb8172cb455caf13d30
kilhyeonjun <kboxstar@gmail.com>
2021-10-09 23:49:02 +0900

    회원 수정 기능 구현

--- src/main/java/com/kbox/blog/controller/api/UserApiController.java
+++ src/main/java/com/kbox/blog/controller/api/UserApiController.java
@@ -1,15 +1,13 @@
 package com.kbox.blog.controller.api;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kbox.blog.dto.ResponseDto;
-import com.kbox.blog.model.RoleType;
 import com.kbox.blog.model.User;
 import com.kbox.blog.service.UserService;
 
@@ -23,6 +21,11 @@ public class UserApiController {
 	public ResponseDto<Integer> save(@RequestBody User user) { // username, password, email
 		System.out.println("UserApiController : save 호출됨");
 		userService.회원가입(user);
+		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1); // 자바오브젝트를 JSON으로 변환해서 리턴 (Jackson)
+	}
+	@PutMapping("/user")
+	public ResponseDto<Integer> update(@RequestBody User user){ // key=value, x-www-form-urlencoded
+		userService.회원수정(user);
 		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1); // 자바오브젝트를 JSON으로 변환해서 리턴 (Jackson)
 	}
 }
