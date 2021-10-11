bd13feae22208fac8ff35ce5e326d109350fbfd1
kilhyeonjun <kboxstar@gmail.com>
2021-10-11 23:43:44 +0900

    회원 수정 기능 세션 등록 추가

--- src/main/java/com/kbox/blog/config/SecurityConfig.java
+++ src/main/java/com/kbox/blog/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.kbox.blog.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -20,6 +21,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Autowired
 	private PrincipalDetailService principalDetialService;
 	
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		// TODO Auto-generated method stub
+		return super.authenticationManagerBean();
+	}
+
 	@Bean // IoC가 됨
 	public BCryptPasswordEncoder encodePWD() {
 		return new BCryptPasswordEncoder();
