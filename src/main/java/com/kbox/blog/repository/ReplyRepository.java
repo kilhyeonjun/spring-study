3a639e8f7abd3c81b0dc8a8c5c29079449fec8e4
kilhyeonjun <kboxstar@gmail.com>
2021-11-01 16:52:11 +0900

    댓글 목록 보여주기

---
+++ src/main/java/com/kbox/blog/repository/ReplyRepository.java
@@ -0,0 +1,9 @@
+package com.kbox.blog.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.kbox.blog.model.Reply;
+
+public interface ReplyRepository extends JpaRepository<Reply, Integer> {
+
+}
