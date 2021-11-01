ce0a239a9043447f4ce950ad83cef7ac54139bb9
kilhyeonjun <kboxstar@gmail.com>
2021-11-01 16:11:42 +0900

    카카오 로그인 서비스 구현 완료

--- src/main/java/com/kbox/blog/controller/UserController.java
+++ src/main/java/com/kbox/blog/controller/UserController.java
@@ -1,9 +1,17 @@
 package com.kbox.blog.controller;
 
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
@@ -14,7 +22,10 @@ import org.springframework.web.client.RestTemplate;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kbox.blog.model.KakaoProfile;
 import com.kbox.blog.model.OAuthToken;
+import com.kbox.blog.model.User;
+import com.kbox.blog.service.UserService;
 
 // 인증이 안된 사용자들이 출입할 수 있는 경로를 /auth/** 허용
 // 그냥 주소가 / 이면 index.jsp 허용
@@ -23,50 +34,53 @@ import com.kbox.blog.model.OAuthToken;
 @Controller
 public class UserController {
 
+	@Value("${kbox.key}")
+	private String kboxKey;
+	
+	@Autowired
+	private AuthenticationManager authenticationManager; 
+	
+	@Autowired
+	private UserService userService;
+
 	@GetMapping("/auth/joinForm")
 	public String joinForm() {
 		return "user/joinForm";
 	}
-	
+
 	@GetMapping("/auth/loginForm")
 	public String loginForm() {
 		return "user/loginForm";
 	}
-	
+
 	@GetMapping("/auth/kakao/callback")
-	public @ResponseBody String kakaoCallback(String code){ // Data를 리턴해주는 컨트롤러 함수
+	public String kakaoCallback(String code) { // Data를 리턴해주는 컨트롤러 함수
 		// POST방식으로 key=value 데이터를 요청 (카카오쪽으로)
 		// Retrofit2
 		// OkHttp
 		// RestTemplate
-		
+
 		RestTemplate rt = new RestTemplate();
-		
+
 		// HttpHeader 오브젝트 생성
 		HttpHeaders headers = new HttpHeaders();
 		headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
-		
+
 		// HttpBody 오브젝트 생성
 		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
 		params.add("grant_type", "authorization_code");
 		params.add("client_id", "e427e7cc129f9d335bcb52983fbbfd9c");
 		params.add("redirect_uri", "http://localhost:8000/auth/kakao/callback");
 		params.add("code", code);
-		
+
 		// HttpHeader와 HttpBody를 하나의 오브젝트에 담기
-		HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest =
-				new HttpEntity<>(params, headers);
-		
+		HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest = new HttpEntity<>(params, headers);
+
 		// Http 요청하기 - Post방식으로 - 그리고 response 변수의 응답 받음.
-		ResponseEntity<String> response = rt.exchange(
-				"https://kauth.kakao.com/oauth/token",
-				HttpMethod.POST,
-				kakaoTokenRequest,
-				String.class
-		);
-		
+		ResponseEntity<String> response = rt.exchange("https://kauth.kakao.com/oauth/token", HttpMethod.POST, kakaoTokenRequest, String.class);
+
 		// Gson, Json Simple, ObjectMapper
-		ObjectMapper objectMapper= new ObjectMapper();
+		ObjectMapper objectMapper = new ObjectMapper();
 		OAuthToken oauthToken = null;
 		try {
 			oauthToken = objectMapper.readValue(response.getBody(), OAuthToken.class);
@@ -75,15 +89,63 @@ public class UserController {
 		} catch (JsonProcessingException e) {
 			e.printStackTrace();
 		}
-		
+
 		System.out.println("카카오 엑세스 토큰 : " + oauthToken.getAccess_token());
+
+		RestTemplate rt2 = new RestTemplate();
+
+		// HttpHeader 오브젝트 생성
+		HttpHeaders headers2 = new HttpHeaders();
+		headers2.add("Authorization", "Bearer " + oauthToken.getAccess_token());
+		headers2.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+		// HttpHeader와 HttpBody를 하나의 오브젝트에 담기
+		HttpEntity<MultiValueMap<String, String>> kakaoProfileRequest = new HttpEntity<>(headers2);
+
+		// Http 요청하기 - Post방식으로 - 그리고 response 변수의 응답 받음.
+		ResponseEntity<String> response2 = rt2.exchange("https://kapi.kakao.com/v2/user/me", HttpMethod.POST, kakaoProfileRequest, String.class);
+
+		ObjectMapper objectMapper2 = new ObjectMapper();
+		KakaoProfile kakaoProfile = null;
+		try {
+			kakaoProfile = objectMapper2.readValue(response2.getBody(), KakaoProfile.class);
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+
+		// User 오브젝트 : username, password, email
+		System.out.println("카카오 아이디(번호) : " + kakaoProfile.getId());
+		System.out.println("카카오 이메일 : " + kakaoProfile.getKakao_account().getEmail());
+
+		System.out.println("블로그서버 유저네임 : " + kakaoProfile.getKakao_account().getEmail() + "_" + kakaoProfile.getId());
+		System.out.println("블로그서버 이메일 : " + kakaoProfile.getKakao_account().getEmail());
+		// UUID란 -> 중복되지 않는 어떤 특정 값을 만들어내는 알고리즘
+		System.out.println("블로그서버 패스워드 : " + kboxKey);
+
+		User KakaoUser = User.builder().username(kakaoProfile.getKakao_account().getEmail() + "_" + kakaoProfile.getId())
+				.password(kboxKey).email(kakaoProfile.getKakao_account().getEmail()).oauth("kakao").build();
+
+		// 가입자 혹은 비가입자 체크 해서 처리
+		User originUser = userService.회원찾기(KakaoUser.getUsername());
+
+		if (originUser.getUsername() == null) {
+			System.out.println("기존 회원이 아닙입니다...!!");
+			userService.회원가입(KakaoUser);
+		}
+
+		System.out.println("자동 로그인 진행합니다.");
+		// 로그인 처리
+		Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(KakaoUser.getUsername(), kboxKey));
+		SecurityContextHolder.getContext().setAuthentication(authentication);
 		
-		return response.getBody();
+		return "redirect:/";
 	}
-	
+
 	@GetMapping("/user/updateForm")
 	public String updateForm() {
 		return "user/updateForm";
 	}
-	
+
 }
--- src/main/java/com/kbox/blog/service/UserService.java
+++ src/main/java/com/kbox/blog/service/UserService.java
@@ -18,7 +18,15 @@ public class UserService {
 
 	@Autowired
 	private BCryptPasswordEncoder encoder;
-	
+
+	@Transactional(readOnly = true)
+	public User 회원찾기(String username) {
+		User user = userRepository.findByUsername(username).orElseGet(() -> {
+			return new User();
+		});
+		return user;
+	}
+
 	@Transactional
 	public void 회원가입(User user) {
 		String rawPassword = user.getPassword();
@@ -33,17 +41,21 @@ public class UserService {
 		// 수정시에는 영속성 컨텍스트 User 오브제그를 영속화 시키고, 영속화된 User 오브젝트를 수정
 		// select를 해서 User오브젝트를 DB로 부터 가져오는 이유는 영속화를 하기 위해서!
 		// 영속화된 오브젝트를 변경하면 자동으로 DB에 update문을 날려줌.
-		User persistance = userRepository.findById(user.getId()).orElseThrow(()->{
+		User persistance = userRepository.findById(user.getId()).orElseThrow(() -> {
 			return new IllegalArgumentException("회원 찾기 실패");
 		});
-		String rawPassword = user.getPassword();
-		String encPassword = encoder.encode(rawPassword);
-		persistance.setPassword(encPassword);
-		persistance.setEmail(user.getEmail());
+
+		// Validate 체크 => oauth에 값이 없으면 수정 가능
+		if (persistance.getOauth() == null || persistance.getOauth().equals("")) {
+			String rawPassword = user.getPassword();
+			String encPassword = encoder.encode(rawPassword);
+			persistance.setPassword(encPassword);
+			persistance.setEmail(user.getEmail());
+		}
 		// 회원 수정 함수 종료시 = 서비스 종료 = 트랜잭션 종료 = commit 이 자동으로 됨.
 		// 영속화된 persistance 객체의 변화가 감지되면 더티체킹이 되어 update문을 날려줌.
 	}
-	
+
 //	@Transactional(readOnly = true) // Select할 때 트랜잭션 시작, 서비스 종료시에 트랜잭션 종료 (정합성)
 //	public User 로그인(User user) {
 //		return userRepository.findByUsernameAndPassword(user.getUsername(), user.getPassword());
