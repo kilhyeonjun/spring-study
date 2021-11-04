7da7f498283900554d073313a5f806ebdf0f31ce
kilhyeonjun <kboxstar@gmail.com>
2021-11-04 17:55:43 +0900

    댓글 작성 구현

--- src/main/java/com/kbox/blog/controller/api/BoardApiController.java
+++ src/main/java/com/kbox/blog/controller/api/BoardApiController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.kbox.blog.config.auth.PrincipalDetail;
 import com.kbox.blog.dto.ResponseDto;
 import com.kbox.blog.model.Board;
+import com.kbox.blog.model.Reply;
+import com.kbox.blog.repository.ReplyRepository;
 import com.kbox.blog.service.BoardService;
 
 @RestController
@@ -21,21 +23,31 @@ public class BoardApiController {
 	@Autowired
 	private BoardService boardServie;
 	
+	@Autowired
+	private ReplyRepository replyRepository;
+
 	@PostMapping("/api/board")
 	public ResponseDto<Integer> save(@RequestBody Board board, @AuthenticationPrincipal PrincipalDetail principal) {
 		boardServie.글쓰기(board, principal.getUser());
 		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
 	}
-	
+
 	@DeleteMapping("/api/board/{id}")
-	public ResponseDto<Integer> deleteById(@PathVariable int id){
+	public ResponseDto<Integer> deleteById(@PathVariable int id) {
 		boardServie.글삭제하기(id);
 		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
 	}
-	
+
 	@PutMapping("/api/board/{id}")
-	public ResponseDto<Integer> update(@PathVariable int id, @RequestBody Board board){
+	public ResponseDto<Integer> update(@PathVariable int id, @RequestBody Board board) {
 		boardServie.글수정하기(id, board);
-		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1); 
+		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
+	}
+
+	@PostMapping("/api/board/{boardId}/reply")
+	public ResponseDto<Integer> replySave(@PathVariable int boardId, @RequestBody Reply reply, @AuthenticationPrincipal PrincipalDetail principal) {
+
+		boardServie.댓글쓰기(principal.getUser(), boardId, reply);
+		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
 	}
 }
--- src/main/java/com/kbox/blog/service/BoardService.java
+++ src/main/java/com/kbox/blog/service/BoardService.java
@@ -7,8 +7,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.kbox.blog.model.Board;
+import com.kbox.blog.model.Reply;
 import com.kbox.blog.model.User;
 import com.kbox.blog.repository.BoardRepository;
+import com.kbox.blog.repository.ReplyRepository;
 
 // 스프링이 컴포넌트 스캔을 통해서 Bean에 등록 해줌. IoC를 해준다.
 @Service
@@ -17,6 +19,9 @@ public class BoardService {
 	@Autowired
 	private BoardRepository boardRepository;
 
+	@Autowired
+	private ReplyRepository replyRepository;
+	
 	@Transactional
 	public void 글쓰기(Board board, User user) { // title, content
 		board.setCount(0);
@@ -44,10 +49,23 @@ public class BoardService {
 	@Transactional
 	public void 글수정하기(int id, Board requestBoard) {
 		Board board = boardRepository.findById(id).orElseThrow(() -> {
-			return new IllegalArgumentException("글 상세보기 실패 : 아이디를 찾을 수 없습니다.");
-		});
+			return new IllegalArgumentException("글 찾기 실패 : 아이디를 찾을 수 없습니다.");
+		}); // 영속화 완료
 		board.setTitle(requestBoard.getTitle());
 		board.setContent(requestBoard.getContent());
 		// 해당 함수로 종료시(Service가 종료될 때) 트랜잭션이 종료됩니다. 이때 더티체킹 - 자동 업데이트가 됨. db flush
+	}
+
+	@Transactional
+	public void 댓글쓰기(User user, int boardId, Reply requestReply) {
+		
+		Board board = boardRepository.findById(boardId).orElseThrow(() -> {
+			return new IllegalArgumentException("댓글 쓰기 실패 : 게시물 id를 찾을 수 없습니다.");
+		}); // 영속화 완료
+		
+		requestReply.setUser(user);
+		requestReply.setBoard(board);
+		
+		replyRepository.save(requestReply);
 	}
 }
