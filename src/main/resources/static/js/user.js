44322b8716a3e9b883ddc5d519ec793a296cac31
kilhyeonjun <kboxstar@gmail.com>
2021-11-05 16:32:06 +0900

    회원가입 중복 에러처리

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -30,9 +30,14 @@ let index = {
 			dataType: 'json', // 요청을 서버로해서 응답이 왔을 때 기본적으로 모든 것이 문자열 (생긴게 json이라면) => javascript오브젝트로 변경
 		})
 			.done(function(resp) {
-				alert('회원가입이 완료되었습니다.');
-				// console.log(resp);
-				location.href = '/';
+				if(resp.status === 500){
+					alert('회원가입이 실패하였습니다.');
+				}else{
+					alert('회원가입이 완료되었습니다.');
+					// console.log(resp);
+					location.href = '/';	
+				}
+				
 			})
 			.fail(function(error) {
 				alert(JSON.stringify(error));
