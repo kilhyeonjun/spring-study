21c034fee968b0fbb7e0d3c97246de580e24f367
kilhyeonjun <kboxstar@gmail.com>
2021-11-08 11:42:34 +0900

    IndexController 핸들러 추가

--- security1/src/main/java/com/kbox/security1/controller/IndexController.java
+++ security1/src/main/java/com/kbox/security1/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.kbox.security1.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller // View를 리턴하겠다
 public class IndexController {
@@ -14,4 +15,36 @@ public class IndexController {
 		// viewReslover 설정 : templates (prefix), .mustach(suffix) 생략 가능
 		return "index"; // src/main/resources/templates/index.mustach
 	}
+	
+	@GetMapping("/user")
+	public @ResponseBody String user() {
+		return "user";
+	}
+	
+	@GetMapping("/admin")
+	public @ResponseBody String admin() {
+		return "admin";
+	}
+	
+	@GetMapping("/manager")
+	public@ResponseBody String manager() {
+		return "manager";
+	}
+	
+	// 스프링시큐리티 해당주소를 낚아챔 - SecurityConfig 파일 생성 후 작동안함.
+	@GetMapping("/login")
+	public @ResponseBody String login() {
+		return "login";
+	}
+	
+	@GetMapping("/join")
+	public @ResponseBody String join() {
+		return "join";
+	}
+	
+	@GetMapping("/joinProc")
+	public @ResponseBody String joinProc() {
+		return "회원가입 완료 됨";
+	}
+	
 }
