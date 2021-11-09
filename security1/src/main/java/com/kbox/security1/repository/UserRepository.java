a759487268c67e704ef461f530433d90b3d99cd2
kilhyeonjun <kboxstar@gmail.com>
2021-11-09 22:13:41 +0900

    로그인 기능 구현

--- security1/src/main/java/com/kbox/security1/repository/UserRepository.java
+++ security1/src/main/java/com/kbox/security1/repository/UserRepository.java
@@ -7,5 +7,9 @@ import com.kbox.security1.model.User;
 // CRUD 함수를 JpaRepository가 들고 있음
 // @Repository라는 어노테이션이 없어도 IoC가 됨. JpaRepository를 상속 했기 때문
 public interface UserRepository extends JpaRepository<User, Integer>{
+
+	// findeBy규칙 -> Username문법
+	// select * from user where username = 1?
+	public User findByUsername(String username); // Jpa Query methods
 	
 }
