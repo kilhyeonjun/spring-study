3b05cfbffe03963707b9deff01ee2ac124c96b77
kilhyeonjun <kboxstar@gmail.com>
2021-12-14 21:29:18 +0900

    Authentication가 가지는 타입 확인(UserDetails, OAuth2User)

--- security1/src/main/java/com/kbox/security1/auth/PrincipalDetails.java
+++ security1/src/main/java/com/kbox/security1/auth/PrincipalDetails.java
@@ -2,11 +2,15 @@ package com.kbox.security1.auth;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Map;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import com.kbox.security1.model.User;
+
+import lombok.Data;
 
 // 시큐리티가 /login 주소 요청이 오면 낚아채서 로그인을 진행시킨다.
 // 로그인 진행이 완료되면 시큐리티 session을 만들어줍니다. (Security ContextHolder)
@@ -16,11 +20,12 @@ import com.kbox.security1.model.User;
 
 // Security Session => Authentication = > UserDetails(PrincipalDetail)
 
-public class PrincipalDetail implements UserDetails {
+@Data
+public class PrincipalDetails implements UserDetails, OAuth2User {
 
 	private User user; // 콤포지션
 
-	public PrincipalDetail(User user) {
+	public PrincipalDetails(User user) {
 		this.user = user;
 	}
 
@@ -70,6 +75,18 @@ public class PrincipalDetail implements UserDetails {
 		// 현재시간 - 로긴시간 => 1년을 초과하면 return false;
 		
 		return true;
+	}
+
+	@Override
+	public Map<String, Object> getAttributes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
--- security1/src/main/java/com/kbox/security1/auth/oauth/PrincipalOauth2UserService.java
+++ security1/src/main/java/com/kbox/security1/auth/oauth/PrincipalOauth2UserService.java
@@ -12,6 +12,12 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService{
 	// 구글로 부터 받은 UserRequest 데이터에 대한 후처리되는 함수
 	@Override
 	public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+
+		// 구글 로그인 버튼 클릭 -> 구글로그이창 -> 로그인 완료 -> code를 리턴(OAuth-Client라이브러리) -> AccessToken 요청
+		// userRequest 정보 -> loadUser 함수 호출 -> 구글로부터 회원프로필 받아준다.
+		
+		OAuth2User oauth2User = super.loadUser(userRequest);
+		
 		// 회원가입 진행 예정
 		return super.loadUser(userRequest);
 	}
