8b455b511f0e676a1a1446e0cc7684eb7663ff1f
kilhyeonjun <kboxstar@gmail.com>
2022-02-23 21:24:05 +0900

    네이버 로그인 구현

--- security1/src/main/java/com/kbox/security1/config/oauth/PrincipalOauth2UserService.java
+++ security1/src/main/java/com/kbox/security1/config/oauth/PrincipalOauth2UserService.java
@@ -1,5 +1,7 @@
 package com.kbox.security1.config.oauth;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
@@ -11,6 +13,7 @@ import org.springframework.stereotype.Service;
 import com.kbox.security1.config.auth.PrincipalDetails;
 import com.kbox.security1.config.oauth.provider.FacebookUserInfo;
 import com.kbox.security1.config.oauth.provider.GoogleUserInfo;
+import com.kbox.security1.config.oauth.provider.NaverUserInfo;
 import com.kbox.security1.config.oauth.provider.OAuth2UserInfo;
 import com.kbox.security1.model.User;
 import com.kbox.security1.repository.UserRepository;
@@ -41,8 +44,10 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService{
 			oAuth2UserInfo = new GoogleUserInfo(oauth2User.getAttributes());
 		}else if(userRequest.getClientRegistration().getRegistrationId().equals("facebook")) {
 			oAuth2UserInfo = new FacebookUserInfo(oauth2User.getAttributes());
+		}else if(userRequest.getClientRegistration().getRegistrationId().equals("naver")) {
+			oAuth2UserInfo = new NaverUserInfo((Map) oauth2User.getAttributes().get("response"));
 		}else {
-			System.out.println("우리는 구글과 페이스북만 지원합니다.");
+			System.out.println("우리는 구글, 페이스북, 네이버만 지원합니다.");
 		}
 		
 		String provider = oAuth2UserInfo.getProvider();
